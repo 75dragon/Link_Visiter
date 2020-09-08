4674b8d06e2c498b72587effce750e5ab5fcd58a
Austin Cheng <austinbicheng@gmail.com>
2020-09-07 22:18:48 -0700

    interval added

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
 import urllib.request
+from apscheduler.schedulers.blocking import BlockingScheduler
 
+def some_job():
+    print("visiting pages")
+    f = open("link.txt", "r")
+    line = f.readline() #first line is #
+    line = f.readline()
+    while line:
+      line = line.rstrip('\n')
+      print("visiting " + line)
+      url = urllib.request.urlopen(line)
+      print("code: " + str(url.getcode()))
+      line = f.readline()
+    f.close()
+
+scheduler = BlockingScheduler()
 f = open("link.txt", "r")
-line = f.readline()
-while line:
-  line = line.rstrip('\n')
-  print("visiting " + line)
-  url = urllib.request.urlopen(line)
-  print("code: " + str(url.getcode()))
-  line = f.readline()
+inter = f.readline().rstrip('\n')
 f.close()
+if not (inter) or not inter.isnumeric():
+    print("forgot time interval on first line, or its not a number")
+else:
+    scheduler.add_job(some_job, 'interval', minutes=int(inter))
+    scheduler.start()
